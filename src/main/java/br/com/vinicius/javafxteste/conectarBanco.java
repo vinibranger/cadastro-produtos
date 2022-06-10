ba44f94e199bb388f4c13b3171d99078f4baf475
vinibranger <vinibranger@gmail.com>
2022-06-09 21:24:27 -0300

    banco dados

---
+++ src/main/java/br/com/vinicius/javafxteste/conectarBanco.java
@@ -0,0 +1,10 @@
+
+package br.com.vinicius.javafxteste;
+
+import java.sql.Connection;
+
+public class conectarBanco {
+    
+  //  Connection
+    
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module br.com.vinicius.javafxteste {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens br.com.vinicius.javafxteste to javafx.fxml;
-    exports br.com.vinicius.javafxteste;
-}
